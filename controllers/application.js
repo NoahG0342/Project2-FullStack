607859247f679e2b48cc57b8c3ba5e413df1364b
CB_ATL_118_Noah_Goodman <cb_atl_118_noah_goodman@MacBook-Air-2.local>
2018-10-26 12:57:27 -0400

    fixed somethings where now my appcontoller is sending things to the local host

---
+++ controllers/application.js
@@ -0,0 +1,7 @@
+const appController = {
+    index: (req, res) => {
+        res.send("hey")
+    }
+}
+
+module.exports = appController
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const appController = require('../controllers/application')
-const platformsController = require('../controllers/platforms')
-const GamesController = require('../controllers/games')
+// const platformsController = require('../controllers/platforms')
+// const GamesController = require('../controllers/games')
 
 ///home page
 router.get('/', appController.index)
 
 ///home page for stores
-router.get('/platforms', platformsController.index)
+// router.get('/platforms', platformsController.index)
 
 module.exports = router
